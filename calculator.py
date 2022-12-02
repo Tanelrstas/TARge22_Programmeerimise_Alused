773c6d72b78a2df36197fdc2146031c55e9c4d52
Opilane TTHK <opilane@tthk.ee>
2022-12-02 17:02:09 +0200

    Add first python files

---
+++ calculator.py
@@ -0,0 +1,11 @@
+
+def main():
+    a = int(input("Number a: "))
+    b = int(input("Number b: "))
+    c = a + b
+    print("a" + " + " + "b = " + str(c))
+    
+if __name__== '__main__' :
+    main()
+    
+    
